0dd30086cae2c4f0ef9064705e718de591f52d40
Mads Jørgensen <mjo@illumi.dk>
2013-05-21 07:03:49 +0200

    Login routing added. Needs to add login screen

--- App/js/app.js
+++ App/js/app.js
@@ -2,11 +2,12 @@
 
 /* App Module */
 
-angular.module('BillysBilling', []).
+angular.module('BillysBilling', ['ngCookies']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/front', {templateUrl: 'partials/frontpage.html',   controller: FrontpageCtrl}).
-      when('/customers', {templateUrl: 'partials/customer-list.html', controller: CustomerListCtrl}).
-      when('/customers/:customerID', {templateUrl: 'partials/customer-detail.html', controller: CustomerDetailCtr}).
-      otherwise({redirectTo: '/front'});
+    when('/login', {templateUrl: 'partials/login.html', controller: LoginCtrl}).
+    when('/front', {templateUrl: 'partials/frontpage.html',   controller: FrontpageCtrl}).
+    when('/customers', {templateUrl: 'partials/customer-list.html', controller: CustomerListCtrl}).
+    when('/customers/:customerID', {templateUrl: 'partials/customer-detail.html', controller: CustomerDetailCtr}).
+    otherwise({redirectTo: '/front'});
 }]);
--- App/js/controllers.js
+++ App/js/controllers.js
@@ -2,11 +2,33 @@
 
 /* Controllers */
 
-function BillysBillingCtrl($scope, $http) {
+function BillysBillingCtrl($scope, $http, $location, $cookies) {
+  // Login handler Checks whenever a page is loaded, is there a login cookie
+
+  // Login handler Checks whenever a page is loaded, is there a login
+  $scope.$watch(function(){
+    return $location.path();
+  },function(newValue, oldValue){
+    if($scope.loggedIn == null && newValue!='/login' && $cookies.login==null) {
+      $location.path('/login');
+    } else {
+      $scope.loggedIn = $cookies.login;
+    }
+  });
+
+
   $http.get('data/sections.json').success(function(data) {
     $scope.sections = data;
   });
 };
+
+function LoginCtrl($scope) {
+  // detect click on submit
+
+  // set cookie and Logged In
+
+  // Redirect to front
+}
 
 function FrontpageCtrl($scope, $routeParams) {
 
